c5d1b6e8449bdab58eae5af4e2a8a4e8454ba146
helder mabasso <hfmabasso@gmail.com>
2024-07-25 23:45:15 +0200

    binaries and obj

--- obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b422df9eee098da4215b7ad4ac7432dc746000e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf6004a63dda5f97af30e05adde7de16e80e575d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
