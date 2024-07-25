f42ff4e0661804ca48114f700f954c9d976e4363
helder mabasso <hfmabasso@gmail.com>
2024-07-25 23:47:35 +0200

    Swagger Services and Swashbuckle package

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
+using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args); //creating a builder
-var app = builder.Build(); //constructing an app instance using builder
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+});
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI(c =>
+   {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+   });
+}
+
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
-
